676a210da51a3b1c5f18afe58b0c3b8df77fc726
Suleman Dawood <sulemandawood123@protonmail.com>
2026-09-15 16:50:47 +1000

    chore(release): pin the Homebrew sha256 to the 1.0.0 tarball
    Computed from npm pack, so the formula installs once 1.0.0 is on the
    registry. Also ignore stray *.tgz files.

--- Formula/githubpill.rb
+++ Formula/githubpill.rb
@@ -4,13 +4,13 @@
 #   brew tap suleman-dawood/githubpill https://github.com/suleman-dawood/githubpill
 #   brew install githubpill
 #
-# After `npm publish`, refresh `url` and `sha256`:
+# The sha256 below is for the 1.0.0 npm tarball. On each release, refresh it:
 #   curl -sL https://registry.npmjs.org/githubpill/-/githubpill-<version>.tgz | shasum -a 256
 class Githubpill < Formula
   desc "Prior-art reconnaissance for project ideas"
   homepage "https://github.com/suleman-dawood/githubpill"
   url "https://registry.npmjs.org/githubpill/-/githubpill-1.0.0.tgz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "1403b0ce3b0859dcb385c0fc0181b5ad043148f73beee02a66a37cb4f54267d6"
   license "MIT"
 
   depends_on "node"
